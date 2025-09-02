b62fa88fa20e3e5ee96c9d33b6014568aa23254f
ariaaaawww <ariantowp0@gmail.com>
2025-09-02 13:49:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Main Page</title>
+</head>
+
+<body>
+    <div class="y">
+        <h1>Main Page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum perspiciatis laboriosam numquam ratione,
+            facere provident similique. Necessitatibus obcaecati culpa cumque, natus fugit dolorum nisi inventore nihil.
+            Blanditiis, dicta. Quas, qui!</p>
+    </div>
+</body>
+
+</html>
