87bbdda914f8197d8e1895bb9eb9efe04fca7afb
bilibili <billychua56@gmail.com>
2025-09-02 14:32:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
     <div class="y">
         <h1>Main Page</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum perspiciatis laboriosam numquam ratione,
+        <p>Lorem ipsum dolor,COMPARE sit amet consectetur adipisicing elit. Harum perspiciatis laboriosam numquam ratione,
             facere provident similique. Necessitatibus obcaecati culpa cumque, natus fugit dolorum nisi inventore nihil.
             Blanditiis, dicta. Quas, qui!</p>
     </div>
